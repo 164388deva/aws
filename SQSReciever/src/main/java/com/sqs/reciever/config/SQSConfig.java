bf4c4c4c13897e63107ad859157ef2ab476a562b
164388_deva <32515992+164388deva@users.noreply.github.com>
2019-06-03 18:18:20 +0530

    Add files via upload
    working code

--- SQSReciever/src/main/java/com/sqs/reciever/config/SQSConfig.java
+++ SQSReciever/src/main/java/com/sqs/reciever/config/SQSConfig.java
@@ -5,8 +5,6 @@ import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
@@ -26,13 +24,13 @@ public class SQSConfig {
 	private String awsSecretKey;
 	
 	@Bean
-	public QueueMessagingTemplate queueMessagingTemplate90() {
+	public QueueMessagingTemplate queueMessagingTemplate() {
 	return new 	QueueMessagingTemplate((amazonSQSAsync()));
 	}
 
 	private AmazonSQSAsync amazonSQSAsync() {
 		// TODO Auto-generated method stub
 		return AmazonSQSAsyncClientBuilder.standard().withRegion(Regions.US_EAST_1)
-				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsAccessKey))).build();
+				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey))).build();
 	}
 	}
--- SQSReciever/src/main/java/com/sqs/reciever/controller/SQSController.java
+++ SQSReciever/src/main/java/com/sqs/reciever/controller/SQSController.java
@@ -6,9 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,13 +23,29 @@ private QueueMessagingTemplate queueMessagingTemplate;
 @Value("${cloud.aws.end-point.url}")
 private String sqsEndpoint;
 
-@GetMapping
-public void sendMessage() {
-	queueMessagingTemplate.send(sqsEndpoint,MessageBuilder.withPayload("Hello from  dev").build());
 	
-}
-@SqsListener("demo")
-public void getMessages(String message) {
-	LOG.info("messagw from sqs" +message);
-}
+	 @GetMapping ("/{personName}")
+	 public void sendMessage(@PathVariable String dev) {
+	  queueMessagingTemplate.send(sqsEndpoint,MessageBuilder.withPayload("Hello from "+dev ).build());
+	  
+	  }
+
+     @GetMapping("/messages/recieved")
+	 public void recieveMessage() {
+    	 
+    	 
+	 String message = queueMessagingTemplate.receiveAndConvert(sqsEndpoint, String.class);
+	 System.out.print(message);
+	 LOG.info("messagw from sqs" +message);
+	 while(message != null) {
+		 
+		message = queueMessagingTemplate.receiveAndConvert(sqsEndpoint, String.class);
+		System.out.print(message);
+		 LOG.info("messagw from sqs" +message);
+	 }  
+	 
+	  
+	  }
+	
+
 }
